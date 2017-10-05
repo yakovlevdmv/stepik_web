ecf067b4e286add09733067418b299bf08eb39df
yakovlevdmv <yakovlevdmv@gmail.ru>
2017-10-05 13:15:02 +0000

    I hate MAIL.RU

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
 def app(environ, start_response):
-        data = b"Hello, World!\n"
-        start_response("200 OK", [
-            ("Content-Type", "text/plain"),
-            ("Content-Length", str(len(data)))
+    data = b"Hello, World!\n"
+    start_response("200 OK", [
+	("Content-Type", "text/plain"),
+        ("Content-Length", str(len(data)))
         ])
-	query = environ['QUERY_STRING']
-	query = query.split('&')
-        return ["{}\n".format(hlib) for hlib in query] #iter([data])
+    query = environ['QUERY_STRING']
+    
+    #print(query)
+    #query = query.split('&')
+    #return iter([''.join(["{}\n".format(hlib) for hlib in query])]) #iter([data])
+    print(query.replace('&', '\n'))
+    return [bytes(query.replace('&','\n'), 'utf-8')]
